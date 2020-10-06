c8e11226c058263b74ef52957e7fa13510fd628b
Bhagi <puppyreddy1997@gmail.com>
2020-10-06 21:36:50 +0530

    [Bhagyalaxmi]Added:adding the postelcode validation tests

--- src/main/java/com/samples/UserValidator.java
+++ src/main/java/com/samples/UserValidator.java
@@ -3,12 +3,12 @@ package com.samples;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-    private static final String FIRST_NAME_PATTEREN = "^[A-Z]{1}[a-zA-Z]{2,}$";
+    private static final String NAME_PATTEREN = "^[A-Z]{1}[a-zA-Z]{2,}$";
     private static final String EMAIL_ADDRESS = "^[a-zA-Z0-9]+(.[a-zA-Z0-9]+)*@[a-zA-Z]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,2})?$";
-    private static final String POSTEL_CODE = "^[1-9]{1}[0-9]{2}[ ][0-9]{3}$";
+    private static final String POSTEL_CODE = "^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$";
 
     public boolean validateFirstName(String fname) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTEREN);
+        Pattern pattern = Pattern.compile(NAME_PATTEREN);
         return pattern.matcher(fname).matches();
     }
 
@@ -20,5 +20,10 @@ public class UserValidator {
     public boolean validatePotelCode(String pincode) {
         Pattern pattern = Pattern.compile(POSTEL_CODE);
         return pattern.matcher(pincode).matches();
+    }
+
+    public boolean validateLastName(String lname) {
+        Pattern pattern = Pattern.compile(NAME_PATTEREN);
+        return pattern.matcher(lname).matches();
     }
 }
--- src/test/java/com/samples/UserValidatorTest.java
+++ src/test/java/com/samples/UserValidatorTest.java
@@ -25,6 +25,43 @@ public class UserValidatorTest {
         boolean result = validator.validateFirstName("bhagya@123");
         Assert.assertFalse(result);
     }
+    @Test
+    public void givenFisrtName_WhenHaveNumberAndSplChar_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("Bhagya@123");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenFisrtName_WhenHaveNumber_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateFirstName("bhagya123");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTure() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Bhagya");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenHaveSplChar_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Bhagy@");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenLastName_WhenHaveNum_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Bhagya123");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenLastName_WhenHaveSplCharAndNum_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validateLastName("Bhagya@123");
+        Assert.assertFalse(result);
+    }
 
     @Test
     public void givenEmailAddress_WhenProper_ShouldReturnTure() {
@@ -46,4 +83,18 @@ public class UserValidatorTest {
         boolean result = validator.validatePotelCode("a45213");
         Assert.assertFalse(result);
     }
+    @Test
+    public void givenPostelCode_WhenHaveNum_ShouldReturnFalse() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validatePotelCode("45213g");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenPostelCode_WhenWithOutSpace_ShouldReturnTure() {
+        UserValidator validator = new UserValidator();
+        boolean result = validator.validatePotelCode("509215");
+        Assert.assertTrue(result);
+    }
+
+
 }
